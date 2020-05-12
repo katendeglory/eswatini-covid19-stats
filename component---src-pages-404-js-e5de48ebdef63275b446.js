bad837db61ad6e15a91b29744353e13c8fd96cbc
Glory Katende <katendeglory@gmail.com>
2020-05-12 17:52:58 +0200

    Updates

---
+++ component---src-pages-404-js-e5de48ebdef63275b446.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),r=n("yBb5");t.default=function(){return l.a.createElement(r.a,null,l.a.createElement("div",{style:{width:"100%",height:"75vh",display:"flex",alignItems:"center",justifyContent:"center"}},l.a.createElement("h2",{className:"sul-box-inset-1",style:{color:"var(--main-text-color)",padding:"5rem 1rem",textAlign:"center"}},"PAGE NOT FOUND ⚠")))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-e5de48ebdef63275b446.js.map
