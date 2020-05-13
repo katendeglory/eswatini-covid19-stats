d40e6a7d7dfe3837e24924f001f5cb78cb7418a9
Glory Katende <katendeglory@gmail.com>
2020-05-13 16:30:46 +0200

    Yet another commit

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -7,7 +7,7 @@ import Loading from './../Utils/Loading';
 import ls from 'local-storage';
 import './Layout.scss';
 
-import { OutboundLink } from 'gatsby-plugin-gtag'
+import { OutboundLink } from 'gatsby-plugin-gtag';
 
 const Layout = (props) => {
 
@@ -26,14 +26,6 @@ const Layout = (props) => {
     <OutboundLink href='https://covid19eswatini.surge.sh/'>
       <div className="layout">
         <Helmet>
-          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-166294029-2"></script>
-          <script>
-            window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'UA-166294029-2');
-        </script>
           <title>Eswatini Covid-19 Stats</title>
           <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
           <link rel="stylesheet" href="https://katendeglory.github.io/soft-ui-library/css/neumorphism-ui.css" />
